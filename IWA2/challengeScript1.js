2be069b643ec344e4a2172bb383ea77f4e9ff417
Malebo29 <126806243+Malebo29@users.noreply.github.com>
2023-07-02 21:56:32 +0200

    separated the javascript files so i can see the outputs of the codes, challengeScripts1.js, challengeScripts2.js, challengeScripts3.js

---
+++ IWA2/challengeScript1.js
@@ -0,0 +1,12 @@
+// Challenge 1
+
+/* This is the firstname of the user */
+
+const user = "John"
+
+/* This is the lastname of the user */
+
+const surname = "Smith"
+
+console.log(user + surname);
+
