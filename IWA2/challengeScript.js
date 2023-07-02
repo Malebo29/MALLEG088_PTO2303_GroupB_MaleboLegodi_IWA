80fff854147c4de89967e52287b303e278d0a652
Malebo29 <126806243+Malebo29@users.noreply.github.com>
2023-07-02 20:13:33 +0200

    created IWA2 folder and challengeScript.js for Challenge 1, 2 and 3 with inline comment and JSDoc comment

---
+++ IWA2/challengeScript.js
@@ -0,0 +1,28 @@
+// Challenge 1
+
+/* This is the firstname of the user */
+
+const user = "John"
+
+/* This is the lastname of the user */ 
+
+const surname = "Smith"
+
+// Challenge 2
+
+/**
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014'
+
+console(date)
+
+console.log(user; surname;)
+
+// Challenge 3
+
+console.warn(Security scan starting)
+
+/* It is important to let user know when they can close the page  */
+
+console.info('Please wait for scan to complete before closing the browser.);
