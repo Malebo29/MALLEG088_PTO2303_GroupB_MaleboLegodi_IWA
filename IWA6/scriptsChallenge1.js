8d4e5fa906ea38a747a60de76f15517125a28a99
Malebo29 <126806243+Malebo29@users.noreply.github.com>
2023-07-05 16:35:07 +0200

    created 2 html and 2javascript pages for the 2 challenges inside the new folder IWA6 created in the repo. copied the challenge1 code from LMS into the challenge1 javascript file.

---
+++ IWA6/scriptsChallenge1.js
@@ -0,0 +1,13 @@
+const primaryPhone = 'O748105141';
+const secondaryPhone = '0219131568';
+
+// Only change below this line
+
+// const primaryValid = typeof number == primaryPhone 
+// const secondaryValid = typeof number == secondaryPhone
+
+const primaryValid = primaryPhone.toString();
+const secondaryValid = secondaryPhone.toString();
+
+console.log('Primary phone is valid numerical string:', primaryValid);
+console.log('Secondary phone is valid numerical string:', secondaryValid);
