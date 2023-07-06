adc5f6c91bb681b8d72ff32eff528eb72aa27444
Malebo29 <126806243+Malebo29@users.noreply.github.com>
2023-07-06 15:36:14 +0200

    After class consultation, we worked out the solution. I updated the code with the correct solution.

--- IWA6/scriptsChallenge1.js
+++ IWA6/scriptsChallenge1.js
@@ -3,11 +3,14 @@ const secondaryPhone = '0219131568';
 
 // Only change below this line
 
-// const primaryValid = typeof number == primaryPhone 
-// const secondaryValid = typeof number == secondaryPhone
-
-const primaryValid = primaryPhone.toString();
-const secondaryValid = secondaryPhone.toString();
-
+const primaryValid = typeof number == primaryPhone 
+// const primaryValid = primaryPhone.toString();
+// const primaryValid = Number(primaryPhone) ? true: false;
+// if(Number(primaryPhone)){primaryValid = true} else {primaryValid = false};
 console.log('Primary phone is valid numerical string:', primaryValid);
+
+// const secondaryValid = secondaryPhone.toString();
+
+// const secondaryValid = typeof Number == secondaryPhone;
+const secondaryValid = Number(secondaryPhone) ? true: false;
 console.log('Secondary phone is valid numerical string:', secondaryValid);
