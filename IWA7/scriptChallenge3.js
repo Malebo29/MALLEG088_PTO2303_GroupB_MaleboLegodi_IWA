6fb88922681a8e1f1c59a86d8fce4847410bbbf7
Malebo29 <126806243+Malebo29@users.noreply.github.com>
2023-07-10 18:46:51 +0200

    updated vs code and was able to use the toFixed method the code was updated and fixed after class review to get a positive value with 2 decimals.

--- IWA7/scriptChallenge3.js
+++ IWA7/scriptChallenge3.js
@@ -15,12 +15,6 @@ const leo = `${leoName} ${leoSurname.trimEnd()} (Owed: R ${(leoBalance*-1).toFix
 const sarah = `${sarahName.trimEnd()} ${sarahSurname} (Owed: R ${(sarahBalance*-1).toFixed(2)})`;
 const total = Number((leoBalance*-1).toFixed(2)) + Number((sarahBalance*-1).toFixed(2));
 const newLine = "\n"
-/* working perfect because you have you 2 divider lines surrounding 
-the STRING - Total amount owed: R 
-*/
-
 const result = leo + newLine + sarah + newLine + newLine + divider + newLine + '  Total amount owed: R ' + total + newLine + divider;
-
-// below is the original console code....the only required console log
 
 console.log(result);
