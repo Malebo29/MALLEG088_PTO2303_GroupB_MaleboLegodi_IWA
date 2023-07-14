1d7b1789d82d8d85c52e4fb2ade60f90662a398f
Malebo29 <126806243+Malebo29@users.noreply.github.com>
2023-07-14 17:34:50 +0200

    fixed up the taxAsDecimal to turn in it into a number using parseInt then interpolated the type variable to match the rent object keys so the correct value can be used to calculate the expenses. Then the after-tax amount is also added as a value for the balance variable.

--- IWA9/scriptChallenge1.js
+++ IWA9/scriptChallenge1.js
@@ -30,8 +30,8 @@ const rent = {
 
 // You can change below however you want
 
-const taxAsDecimal = tax.913 / 100
-const startingAfterTax = salary * 1 - taxAsDecimal
-const type = lodging + size
-const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
-console.log(balance)
+const taxAsDecimal = salary * (parseInt(tax['913']) / 100);
+const startingAfterTax = salary * 1 - taxAsDecimal;
+const type = `${size} ${lodging}`;
+const balance = startingAfterTax - (expenses.transport - expenses.food) - rent['large-apartment'];
+console.log(balance.toFixed(2))
