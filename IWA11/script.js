0754bfccb05e1bc198ecea0cf35806db145ac0b6
Malebo29 <126806243+Malebo29@users.noreply.github.com>
2023-08-08 13:58:42 +0200

    called the method on the orders variables (order1, order2 and order3) to access the items of each order. From each order element, accessed the 1st 2nd 3rd 4th children then their first children to assign the data attribute value for each order item.

--- IWA11/script.js
+++ IWA11/script.js
@@ -1,50 +1,40 @@
-const order1 = document.querySelector('[data-key="order1"]'); // checks for the value of the date-key
-console.log(order1.dataset);
-console.log(order1.dataset.biscuits); // get the property by the part of the attribute name after data-
-console.log(typeof order1);
+const order1 = document.querySelector('[data-key="order1"]'); // used the querySelector method to access data attributes of the three order groups
+console.log(order1);
+// console.log(order1.dataset.biscuits); // get the property by the part of the attribute name after data-
+// console.log(typeof order1);
 
-// const biscuits1 = document.querySelector(order1.dataset.biscuits);
-const biscuits1 = document.querySelector('.biscuits');
-const donuts1 = document.querySelector('.donuts');
-const pancakes1 = document.querySelector('.pancakes');
-const status1 = document.querySelector('.delivered');
+const biscuits1 = order1.getAttribute('data-biscuits');
+const donuts1 = order1.getAttribute('data-donuts');
+const pancakes1 = order1.getAttribute('data-pancakes');
+const status1 = order1.getAttribute('data-delivered');
 
-console.log(biscuits1);
-
-
+order1.children[0].children[1].innerHTML = biscuits1
+order1.children[1].children[1].innerHTML = donuts1
+order1.children[2].children[1].innerHTML = pancakes1
+order1.children[3].children[1].innerHTML = status1 ? 'Delivered' : 'Pending'
 
 const order2 = document.querySelector('[data-key="order2"]');
-console.log(order2.dataset);
-console.log(typeof order2);
+console.log(order2);
 
-const biscuits2 = document.querySelector('.biscuits');
-const donuts2 = document.querySelector('.donuts');
-const pancakes2 = document.querySelector('.pancakes');
-const status2 = document.querySelector('.delivered');
+const biscuits2 = order2.getAttribute('data-biscuits');
+const donuts2 = order2.getAttribute('data-donuts');
+const pancakes2 = order2.getAttribute('data-pancakes');
+const status2 = order2.getAttribute('data-delivered');
 
-console.log(biscuits2);
+order2.children[0].children[1].innerHTML = biscuits2
+order2.children[1].children[1].innerHTML = donuts2
+order2.children[2].children[1].innerHTML = pancakes2
+order2.children[3].children[1].innerHTML = status1 ? 'Delivered' : 'Pending'
 
 const order3 = document.querySelector('[data-key="order3"]');
-console.log(order3.dataset);
-console.log(typeof order2);
+console.log(order3);
 
-const biscuits3 = document.querySelector('.biscuits');
-const donuts3 = document.querySelector('.donuts');
-const pancakes3 = document.querySelector('.pancakes');
-const status3 = document.querySelector('.delivered');
+const biscuits3 = order3.getAttribute('data-biscuits');
+const donuts3 = order3.getAttribute('data-donuts');
+const pancakes3 = order3.getAttribute('data-pancakes');
+const status3 = order3.getAttribute('data-delivered');
 
-
-biscuits1.order1.biscuits;
-donuts1.order1.donuts;
-pancakes1.pancakes;
-status1 = order1.status ? Delivered : Pending;
-
-biscuits2 = order2.biscuits;
-donuts2 = order2.donuts;
-pancakes2 = order2.pancakes;
-status2 = order2.status ? Delivered : Pending;
-
-biscuits3 = order3.biscuits;
-donuts3 = order3.donuts;
-pancakes3 = order3.pancakes;
-status3 = order3.status ? Delivered : Pending;
+order3.children[0].children[1].innerHTML = biscuits3
+order3.children[1].children[1].innerHTML = donuts3
+order3.children[2].children[1].innerHTML = pancakes3
+order3.children[3].children[1].innerHTML = status3 ? 'Delivered' : 'Pending'
