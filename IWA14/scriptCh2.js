ea1297e196b97b7dc953ee5ee2f23bee7365c3f1
Malebo29 <126806243+Malebo29@users.noreply.github.com>
2023-08-09 17:49:54 +0200

    converted the add and multiply fuctions into an arrow function by removing the function keyword then assigned the function to add and multiply variables.

--- IWA14/scriptCh2.js
+++ IWA14/scriptCh2.js
@@ -2,26 +2,44 @@
 // multiple parameters
 
 const add = (a, b) => a + b
-
 const multiply = (a, b) => a - b
+
+// function add(a, b) {
+// 	return a + b;
+//   }
+
+//   function multiply(a, b) {
+// 	return a - b;
+//   }
+
+console.log(add (3, 7));
+console.log(multiply (8, 2));
+
+function internal (add, multiply) {
+	this.add = (this.a + this.b)
+	this.multiply = (this.a - this.b)
+	return internal;
+}
+
+// function Internal () {					// Constructor Function used to create objects
+// 	const added = this.add(this.a + this.b)
+// 	this.multiply(this.a - this.b)
+// 	return added;
+// }
 
 // const internal = () => {
 // 	const added = this.add(this.a, this.b)
-// 	const multiplied = this.multiply(this.a, this.b)
-// 	return added + multiplied;
-// }
-
-// const internal = () => {
-// 	this.add(this.a, this.b)
 // 	this.multiply(this.a, this.b)
-// 	return internal;
+// 	return added;
 // }
 
-function internal () {
-	this.add(this.a, this.b)
-	this.multiply(this.a, this.b)
-	return internal;
-}
+console.log(internal());
+
+// function internal () {
+// 	this.add = (this.a, this.b)
+// 	this.multiply = (this.a, this.b)
+// 	// return internal;
+// }
 
 // Not allowed to change below this
 
