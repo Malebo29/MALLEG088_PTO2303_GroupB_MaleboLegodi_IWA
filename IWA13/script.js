0fa41171181ce5abdb60a28fc597e938d23e8a12
Malebo29 <126806243+Malebo29@users.noreply.github.com>
2023-08-10 16:59:38 +0200

    because local scope variables can only be used within the function they are defined into, the global calculated variable was assigned the value of the local scope variables then returned so it can be used outside of the function block.

--- IWA13/script.js
+++ IWA13/script.js
@@ -5,32 +5,29 @@ let calculated = '1'
 // Only allowed to change below
 
 const logCalc = () => { 
-    const isString = typeof calculated !== 'numerical-string'; // used the Is Not !== to make a true value
-    const calculatedAsNumber = isString ? calculated : calculated;
-    parseInt(calculated) + parseInt(calculatedAsNumber) * 1
-    return logCalc;
+    const isString = typeof calculated == 'numerical-string';
+    const calculatedAsNumber = isString ? calculated : parseInt(calculated);      
+    calculated = calculatedAsNumber + 1 
+    return calculated;
 };
 
 const calcUser = () => {
   logCalc();
   if (calculated > 2) {
-    user === 'John'
+    user = 'John'
   };
   if (calculated > 2) {
-    state === 'requesting'
+    state = 'requesting'
   };
   if (calculated > 3) {
-    state === 'idle'
+    state = 'idle'
   };
-  return calcUser;
 };
 
 const checkUser = () => {
-  calcUser()
-	if (user === 'John' && state === 'requesting') {
+	if (user == 'John' && state == 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
 	}
-  return checkUser;
 };
 
 // Only allowed to change code above
