407b100cfa3835e446adb877e6a34f1a82158fce
Malebo29 <126806243+Malebo29@users.noreply.github.com>
2023-08-24 01:21:04 +0200

    after several attempts, i managed to work out how the this keyword is supposed to work and that the minus was supposed to be changed to a multiple sign.

--- IWA14/scriptCh2.js
+++ IWA14/scriptCh2.js
@@ -1,30 +1,11 @@
-// because we are adding a + b together, the variable 'a' and 'b' for both functions are put in brackets to pass them inside their functions
-// multiple parameters
-
 const add = (a, b) => a + b
-const multiply = (a, b) => a - b
+const multiply = (a, b) => a * b
 
-// function Internal() {
-//  this.add = add // (this.a, this.b)
-//  this.multiply = multiply // (this.a, this.b)
-//  return this
-// }
-
-// function Internal() {
-//   this.add = this.a + this.b
-//   this.multiply = this.a - this.b
-//       return this
-//   }
-
-function internal (add, multiply) {					// Constructor Function used to create objects
-	this.add = add(this.a, this.b);
-    this.multiply = multiply(this.a, this.b);
-	return this;
+function internal () {
+	const added = add(this.internal.a, this.internal.b);
+    const results = multiply(added, this.internal.c);
+	return results;
 }
-
-console.log(internal())
-
-// const calculate = internal();
 
 // Not allowed to change below this
 
